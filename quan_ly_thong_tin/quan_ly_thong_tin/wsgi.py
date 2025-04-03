1fa36039fe49ff20eb15e70d9e175e4a3d97620e
nghiaugust <115493863+nghiaugust@users.noreply.github.com>
2025-04-03 20:59:25 +0700

    migration

--- quan_ly_thong_tin/quan_ly_thong_tin/wsgi.py
+++ quan_ly_thong_tin/quan_ly_thong_tin/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quan_ly_thong_tin.settings')
 
 application = get_wsgi_application()
+try:
+    from django.core.management import call_command
+    call_command('migrate')
+except Exception as e:
+    print(f"Migration error: {e}")
